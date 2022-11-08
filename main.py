911fa988a11ad36236e24bf6a9c3601360ab44b4
Imismim <imismims@gmail.com>
2022-11-08 22:34:29 +0200

    palindrome funktion + list uniq elements

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class Func:
     def checkout_palindrome(str_input: str) -> bool:
         return str_input == str_input[::-1]
 
+    @staticmethod
+    def list_uniq_elements(list_input: list) -> list:
+        return list(set(list_input))
+
 
 if __name__ == '__main__':
     print(Func.checkout_palindrome("111"))
+    print(Func.list_uniq_elements([1,1,1,23213,123]))
