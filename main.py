2996ee477f588ab43cadf5eb7c32af8a885c458e
Imismim <imismims@gmail.com>
2022-11-08 23:09:34 +0200

    added count word method

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ class Func:
     def list_uniq_elements(list_input: list) -> list:
         return list(set(list_input))
 
+    @staticmethod
+    def count_words(text_input: str) -> dict:
+        import string
+        return {
+            "count words": len(text_input.split()),
+            "count different symbols": len([i for i in text_input if i in string.punctuation])
+        }
+
 
 if __name__ == '__main__':
     print(Func.checkout_palindrome("111"))
-    print(Func.list_uniq_elements([1,1,1,23213,123]))
+    print(Func.list_uniq_elements([1, 1, 1, 23213, 123]))
+    print(Func.count_words("I am learning git and github. Ohoo!"))
